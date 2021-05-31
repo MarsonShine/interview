12ad0e983fbab8e59dc7023fdc8a2dacfa57db2c
marsonshine <ms27946@163.com>
2021-05-31 17:10:47 +0800

    feat: mysql 面试题，go 面试题

--- go/main.go
+++ go/main.go
@@ -2,67 +2,68 @@ package main
 
 import (
 	"fmt"
-	iv "interview/src"
+	"reflect"
 	"sync"
 	"time"
+	"unsafe"
 )
 
 func main() {
-	m := map[string]iv.Student{"people": {"marsonshine"}}
-	// m1 := map[string]iv.Student{"people": {"marsonshine"}}
-	// m["people"].Name = "marsonshine"
-	fmt.Printf("%v", m)
+	// m := map[string]iv.Student{"people": {"marsonshine"}}
+	// // m1 := map[string]iv.Student{"people": {"marsonshine"}}
+	// // m["people"].Name = "marsonshine"
+	// fmt.Printf("%v", m)
 
-	ret := iv.GoExec("111", func(n string) string {
-		return n + "func1"
-	}, func(n string) string {
-		return n + "func2"
-	}, func(n string) string {
-		return n + "func3"
-	}, func(n string) string {
-		return n + "func4"
-	})
-	fmt.Println(ret)
+	// ret := iv.GoExec("111", func(n string) string {
+	// 	return n + "func1"
+	// }, func(n string) string {
+	// 	return n + "func2"
+	// }, func(n string) string {
+	// 	return n + "func3"
+	// }, func(n string) string {
+	// 	return n + "func4"
+	// })
+	// fmt.Println(ret)
 
-	// iv.Dead()
+	// // iv.Dead()
 
-	for _, i := range [5]int{1, 2, 3, 4, 5} {
-		fmt.Print(i)
-	}
-	wg := sync.WaitGroup{}
-	wg.Add(5)
-	for _, i := range [5]int{1, 2, 3, 4, 5} {
-		go func() {
-			fmt.Println(i)
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-	wg.Add(5)
-	fmt.Println("")
-	for _, i := range [5]int{1, 2, 3, 4, 5} {
-		go func(i int) {
-			fmt.Println(i)
-			wg.Done()
-		}(i)
-	}
-	wg.Wait()
+	// for _, i := range [5]int{1, 2, 3, 4, 5} {
+	// 	fmt.Print(i)
+	// }
+	// wg := sync.WaitGroup{}
+	// wg.Add(5)
+	// for _, i := range [5]int{1, 2, 3, 4, 5} {
+	// 	go func() {
+	// 		fmt.Println(i)
+	// 		wg.Done()
+	// 	}()
+	// }
+	// wg.Wait()
+	// wg.Add(5)
+	// fmt.Println("")
+	// for _, i := range [5]int{1, 2, 3, 4, 5} {
+	// 	go func(i int) {
+	// 		fmt.Println(i)
+	// 		wg.Done()
+	// 	}(i)
+	// }
+	// wg.Wait()
 
-	// 组合
-	t := iv.Teacher{}
-	t.ShowA()
+	// // 组合
+	// t := iv.Teacher{}
+	// t.ShowA()
 
-	// defer 调用时参数作用域
-	a := 1
-	b := 2
-	defer calc("1", a, calc("10", a, b))
-	a = 0
-	defer calc("2", a, calc("20", a, b))
-	b = 1
+	// // defer 调用时参数作用域
+	// a := 1
+	// b := 2
+	// defer calc("1", a, calc("10", a, b))
+	// a = 0
+	// defer calc("2", a, calc("20", a, b))
+	// b = 1
 
-	s := make([]int, 5) // make 指定了长度，追加会从 len(length) 处填充数据
-	s = append(s, 1, 2, 3)
-	fmt.Println(s) // 所以会显示 0 0 0 0 0 1 2 3
+	// s := make([]int, 5) // make 指定了长度，追加会从 len(length) 处填充数据
+	// s = append(s, 1, 2, 3)
+	// fmt.Println(s) // 所以会显示 0 0 0 0 0 1 2 3
 
 	// RWMutx
 	// go A()
@@ -84,26 +85,68 @@ func main() {
 	// fmt.Println(mu.count, mu2.count)
 
 	// pool
-	iv.Pool()
+	// iv.Pool()
 
-	// 定时器
-	go func() {
-		t := time.NewTicker(time.Second * 1)
-		for {
-			select {
-			case <-t.C:
-				go func() {
-					defer func() {
-						if err := recover(); err != nil {
-							fmt.Println(err)
-						}
-					}()
-					proc()
-				}()
-			}
-		}
-	}()
-	select {}
+	// channel
+	// var ch chan int
+	// go func() {
+	// 	// ch = make(chan int, 1)
+	// 	ch <- 1
+	// }()
+	// go func(ch chan int) {
+	// 	time.Sleep(time.Second)
+	// 	<-ch
+	// }(ch)
+	// c := time.Tick(1 * time.Second)
+	// for range c {
+	// 	fmt.Printf("#goroutines: %d\n", runtime.NumGoroutine())
+	// }
+
+	//
+	// go f()
+	// c <- 0
+	// print(a)
+
+	// // 定时器
+	// go func() {
+	// 	t := time.NewTicker(time.Second * 1)
+	// 	for {
+	// 		select {
+	// 		case <-t.C:
+	// 			go func() {
+	// 				defer func() {
+	// 					if err := recover(); err != nil {
+	// 						fmt.Println(err)
+	// 					}
+	// 				}()
+	// 				proc()
+	// 			}()
+	// 		}
+	// 	}
+	// }()
+	// select {}
+
+	// 内存逃逸分析
+	a := foo("hello")
+	b := a.s + " world"
+	c := b + "!"
+	fmt.Println(c)
+
+	// 零拷贝 string - byte 转换
+	aaa := "aaa"
+	ssh := *(*reflect.StringHeader)(unsafe.Pointer(&aaa)) // 可以把字符串 aaa 转成底层结构的形式。
+	bbb := *(*[]byte)(unsafe.Pointer(&ssh))               // 可以把 ssh 底层结构体转成 byte 的切片的指针。
+	fmt.Printf("%v", bbb)                                 // 再通过 * 转为指针指向的实际内容。
+}
+
+type AA struct {
+	s string
+}
+
+func foo(s string) *AA {
+	a := new(AA)
+	a.s = s
+	return a
 }
 func proc() {
 	panic("ok")
@@ -134,4 +177,12 @@ func C() {
 type MyMutex struct {
 	count int
 	sync.Mutex
+}
+
+var c = make(chan int)
+var a int
+
+func f() {
+	a = 1
+	<-c
 }
--- go/src/maps.go
+++ go/src/maps.go
@@ -1,1 +1,12 @@
 package interview
+
+import (
+	"sync"
+)
+
+func Map() {
+	var m sync.Map
+	m.LoadOrStore("a", 1)
+	m.Delete("a")
+	// fmt.Println(m.Len())
+}
